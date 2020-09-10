416a7f0a5be4a1e44ebce7f2450832ab6bd30cc7
heqingtong <heqingtong@foxmail.com>
2020-09-10 17:14:35 +0800

    1

--- src/store/modules/permission.js
+++ src/store/modules/permission.js
@@ -71,10 +71,10 @@ const permission = {
         const { access } = data
         const map = asyncRouterMap
         // const accessedRouters = mapAsyncRouter(map, access)
-        const accessedRouters = asyncRouterMap
-        accessedRouters.push({ path: '*', redirect: '/404', hidden: true })
-        commit('SET_ROUTERS', accessedRouters)
-        resolve()
+        // const accessedRouters = asyncRouterMap
+        // accessedRouters.push({ path: '*', redirect: '/404', hidden: true })
+        // commit('SET_ROUTERS', accessedRouters)
+        // resolve()
       })
     }
   }
